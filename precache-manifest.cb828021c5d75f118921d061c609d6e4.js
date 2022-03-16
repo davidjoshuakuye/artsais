130b4a9fd6201eb15c0c6790f279da090e9c886f
Shemang David <davidshemang@gmail.com>
2022-03-16 16:10:01 +0100

    Updates

---
+++ precache-manifest.cb828021c5d75f118921d061c609d6e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d33f95f8079dc1849ac78ee373838a6",
+    "url": "/artsais/index.html"
+  },
+  {
+    "revision": "b43a84471cd92c4de442",
+    "url": "/artsais/static/css/2.a4c8f85b.chunk.css"
+  },
+  {
+    "revision": "7cab5dfa48c96fec96b6",
+    "url": "/artsais/static/css/main.3081a8c7.chunk.css"
+  },
+  {
+    "revision": "b43a84471cd92c4de442",
+    "url": "/artsais/static/js/2.58d54896.chunk.js"
+  },
+  {
+    "revision": "2580c690bf9c171eab715e0e5a1c766e",
+    "url": "/artsais/static/js/2.58d54896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cab5dfa48c96fec96b6",
+    "url": "/artsais/static/js/main.45e7ef71.chunk.js"
+  },
+  {
+    "revision": "d3e4f38b547bb13751ac",
+    "url": "/artsais/static/js/runtime-main.2160dd88.js"
+  }
+]);
